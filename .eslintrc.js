a095dc0d6ea932a6c920ddf04a49f43e48179db2
Rikuto Yamaguchi <yamaguchirkt@gmail.com>
2020-12-11 16:53:29 +0900

    comment

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ src/components/AppButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'react-native';
+
+type AppButtonPropsType = {
+  title: string;
+  onPress: () => void;
+};
+
+export const AppButton: React.FunctionComponent<AppButtonPropsType> = (
+  props: AppButtonPropsType,
+) => <Button title={props.title} onPress={props.onPress} />;
